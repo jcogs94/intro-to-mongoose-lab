475becd770d30696a5954f79b8bb388e5a7b7952
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-24 14:39:57 -0400

    database added

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-const prompt = require('prompt-sync')();
+const dotenv = require('dotenv');
+dotenv.config();
 
-const username = prompt('What is your name? ');
+const mongoose = require('mongoose');
+const Customer = require('./models/customer.js');
 
-console.log(`Your name is ${username}`);
+const connect = async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Connected to database...');
+    await runQueries();
+    await mongoose.disconnect();
+    console.log('Disconnected from database...');
+}
+
+const runQueries = async () => {
+    console.log('Queries running...');
+
+
+    // const prompt = require('prompt-sync')();
+    // const username = prompt('What is your name? ');
+    // console.log(`Your name is ${username}`);
+}
+
+connect();
