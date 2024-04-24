7dc174ae91ad70ae9ac7116145952e46975fa4ec
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-24 15:05:11 -0400

    customer model added, user prompt and input validation added

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const dotenv = require('dotenv');
 dotenv.config();
-
 const mongoose = require('mongoose');
 const Customer = require('./models/customer.js');
+const prompt = require('prompt-sync')();
+
 
 const connect = async () => {
     await mongoose.connect(process.env.MONGODB_URI);
@@ -12,13 +13,51 @@ const connect = async () => {
     console.log('Disconnected from database...');
 }
 
+// Validates user input by determining it is an integer
+// between 1-5
+const validateInput = (input) => {
+    if (parseInt(input) != NaN && 6 > parseInt(input) && parseInt(input) > 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Displays user prompt and repeats until a valid input
+// is given
+const userPrompt = () => {
+    let validInput = false;
+    console.clear();
+    
+    console.log('Welcome to the CRM\n');
+    console.log('What would you like to do?\n');
+    console.log('  1. Create a customer');
+    console.log('  2. View all customers');
+    console.log('  3. Update a customer');
+    console.log('  4. Delete a customer');
+    console.log('  5. Quit\n');
+    
+    while (validInput === false) {
+        let userInput = prompt('Number of action you would like to run: ');
+        
+        if (validateInput(userInput)) {
+            validInput = true;
+        } else {
+            console.log('Invalid input. Try again.');
+        }
+    }
+}
+
 const runQueries = async () => {
     console.log('Queries running...');
 
-
-    // const prompt = require('prompt-sync')();
     // const username = prompt('What is your name? ');
     // console.log(`Your name is ${username}`);
 }
 
-connect();
+const startApp = async () => {
+    userPrompt();
+    // connect();
+}
+
+startApp();
